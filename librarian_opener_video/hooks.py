e09d3c1c075eb6e311ca44615e9bb7769e4f427c
Andrean Franc <andrean.franc@gmail.com>
2015-10-02 18:08:15 +0200

    Adjust to new opener registration method

--- librarian_opener_video/hooks.py
+++ librarian_opener_video/hooks.py
@@ -2,5 +2,6 @@ from .opener import video_opener
 
 
 def initialize(supervisor):
-    extensions = supervisor.config['video.extensions']
-    supervisor.exts.openers.register('video', video_opener, extensions)
+    supervisor.exts.openers.register('video',
+                                     video_opener,
+                                     content_type='video')
