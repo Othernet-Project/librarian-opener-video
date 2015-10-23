013a89dd09b7fb34106cc3778b244c6189ea9d09
Andrean Franc <andrean.franc@gmail.com>
2015-10-23 13:56:45 +0200

    Register opener label

--- librarian_opener_video/hooks.py
+++ librarian_opener_video/hooks.py
@@ -1,7 +1,10 @@
+from bottle_utils.i18n import lazy_gettext as _
+
 from .opener import video_opener
 
 
 def initialize(supervisor):
     supervisor.exts.openers.register('video',
-                                     video_opener,
+                                     label=_("Video"),
+                                     route=video_opener,
                                      content_type='video')
