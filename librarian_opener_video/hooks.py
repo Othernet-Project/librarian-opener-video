ad3711b09f66fa5d3c24f0a912c6b7b436e99f8f
Branko Vukelic <branko@outernet.is>
2015-10-26 14:35:13 +0100

    Changed opener label

--- librarian_opener_video/hooks.py
+++ librarian_opener_video/hooks.py
@@ -5,6 +5,6 @@ from .opener import video_opener
 
 def initialize(supervisor):
     supervisor.exts.openers.register('video',
-                                     label=_("Video"),
+                                     label=_("Watch"),
                                      route=video_opener,
                                      content_type='video')
