a9a6f66bd173cdd1d5a1065491687ca6895737b3
Andrean Franc <andrean.franc@gmail.com>
2015-10-27 17:15:51 +0100

    Use quoted url form

--- librarian_opener_video/opener.py
+++ librarian_opener_video/opener.py
@@ -1,6 +1,6 @@
 import os
 
-from bottle import request
+from bottle_utils.html import quoted_url
 
 from librarian_content.decorators import with_meta
 from librarian_core.contrib.templates.renderer import view
@@ -10,12 +10,12 @@ from librarian_core.contrib.templates.renderer import view
 @with_meta(abort_if_not_found=False)
 def video_opener(path, meta):
     if not meta:
-        return dict(video_url=request.app.get_url('files:direct', path=path),
+        return dict(video_url=quoted_url('files:direct', path=path),
                     width='100%',
                     height='100%')
 
     resolution = (meta['video'].get('resolution') or 'x').split('x')
     width, height = [int(x) if x else '100%' for x in resolution]
     file_path = os.path.join(meta.path, meta['video']['main'])
-    video_url = request.app.get_url('files:direct', path=file_path)
+    video_url = quoted_url('files:direct', path=file_path)
     return dict(video_url=video_url, width=width, height=height)
